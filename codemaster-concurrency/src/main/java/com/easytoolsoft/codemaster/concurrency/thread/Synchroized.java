cab00adb4c2c577df3adfdea445d3e967c19bc3f
tomdeng <14068728@qq.com>
2016-10-17 21:05:56 +0800

    搞明白Synchroized

--- codemaster-concurrency/src/main/java/com/easytoolsoft/codemaster/concurrency/thread/Synchroized.java
+++ codemaster-concurrency/src/main/java/com/easytoolsoft/codemaster/concurrency/thread/Synchroized.java
@@ -20,18 +20,24 @@ public class Synchroized {
 //        Thread task2t2 = new Thread(() -> task2.m4t2(), "t2");
 //        task2t1.start();
 //        task2t2.start();
-//
-        Task3 task3 = new Task3();
-        Thread task3t1 = new Thread(() -> task3.m4t1(), "t1");
-        Thread task3t2 = new Thread(() -> task3.m4t2(), "t2");
-        task3t1.start();
-        task3t2.start();
+
+//        Task3 task3 = new Task3();
+//        Thread task3t1 = new Thread(() -> task3.m4t1(), "t1");
+//        Thread task3t2 = new Thread(() -> task3.m4t2(), "t2");
+//        task3t1.start();
+//        task3t2.start();
 //
 //        Task4 task4 = new Task4();
 //        Thread task4t1 = new Thread(() -> task4.m4t1(), "t1");
 //        Thread task4t2 = new Thread(() -> task4.m4t2(), "t2");
 //        task4t1.start();
 //        task4t2.start();
+
+        Task5 task5 = new Task5();
+        Thread task5t1 = new Thread(() -> task5.m4t1(), "t1");
+        Thread task5t2 = new Thread(() -> task5.m4t2(), "t2");
+        task5t1.start();
+        task5t2.start();
     }
 }
 
@@ -52,7 +58,7 @@ class Task2 {
             while (i-- > 0) {
                 System.out.println(Thread.currentThread().getName() + " : " + i);
                 try {
-                    Thread.sleep(0);
+                    Thread.sleep(500);
                 } catch (InterruptedException ie) {
                 }
             }
@@ -64,7 +70,7 @@ class Task2 {
         while (i-- > 0) {
             System.out.println(Thread.currentThread().getName() + " : " + i);
             try {
-                Thread.sleep(0);
+                Thread.sleep(500);
             } catch (InterruptedException ie) {
             }
         }
@@ -114,6 +120,35 @@ class Task4 {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException ie) {
+            }
+        }
+    }
+}
+
+class Task5 {
+    private byte[] lock = new byte[0];
+    public void m4t1() {
+        synchronized (this) {
+            int i = 5;
+            while (i-- > 0) {
+                System.out.println(Thread.currentThread().getName() + " : " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ie) {
+                }
+            }
+        }
+    }
+
+    public void m4t2() {
+        synchronized (lock) {
+            int i = 5;
+            while (i-- > 0) {
+                System.out.println(Thread.currentThread().getName() + " : " + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ie) {
+                }
             }
         }
     }
