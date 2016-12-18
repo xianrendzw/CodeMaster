a8773acd9bac95b8a8eab8dd4003d2b543dfe942
tomdeng <14068728@qq.com>
2016-12-18 22:12:24 +0800

    update

--- codemaster-bat50question/src/main/java/tech/codemaster/bat50question/basic/FloatPointNumber.java
+++ codemaster-bat50question/src/main/java/tech/codemaster/bat50question/basic/FloatPointNumber.java
@@ -1,6 +1,6 @@
 package tech.codemaster.bat50question.basic;
 
-import tech.codemaster.common.converter.BitConverter;
+import java.math.BigDecimal;
 
 /**
  * 浮点数运算、精度丢失
@@ -8,9 +8,12 @@ import tech.codemaster.common.converter.BitConverter;
  */
 public class FloatPointNumber {
     public static void main(String[] args){
-        System.out.println(BitConverter.getBits(0.4));
-        System.out.println(BitConverter.getBits(0.2 + 0.2));
-        System.out.println(BitConverter.getBits(0.4 + 0.2));
-        System.out.println(BitConverter.getBits(0.5));
+//        System.out.println(BitConverter.getBits(0.4));
+//        System.out.println(BitConverter.getBits(0.2 + 0.2));
+//        System.out.println(BitConverter.getBits(0.4 + 0.2));
+//        System.out.println(BitConverter.getBits(0.5));
+        BigDecimal a = new BigDecimal("0.2");
+        BigDecimal b = new BigDecimal("0.4");
+        System.out.println(a.add(b).doubleValue());
     }
 }
